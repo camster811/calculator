db41c46756719c53de3ec0c4e191db0f376d0159
Camster811 <cls0150@auburn.edu>
2024-06-05 23:58:15 -0500

    fully flesh out functionality

--- script.js
+++ script.js
@@ -1,49 +1,82 @@
 let firstNum;
 let secondNum;
 let operator;
+let equalsClicked = false;
 const display = document.querySelector('p');
 const numberButtons = document.querySelectorAll('.number')
 const operatorButtons = document.querySelectorAll('.operator')
-
-const one = document.getElementById('1');
-const two = document.getElementById('2');
-const three = document.getElementById('3');
-const four = document.getElementById('4');
-const five = document.getElementById('5');
-const six = document.getElementById('6');
-const seven = document.getElementById('7');
-const eight = document.getElementById('8');
-const nine = document.getElementById('9');
-const clearButton = document.getElementById('clear');
+const equal = document.getElementById('equals');
+const clearAll = document.getElementById('clear');
 const del = document.getElementById('delete');
-const percent = document.getElementById('%');
-const division = document.getElementById('/');
-const multiplication = document.getElementById('x');
-const subtraction = document.getElementById('-');
-const addition = document.getElementById('+');
+const pct = document.getElementById('%');
+const decimal = document.getElementById('decimal');
 
 function operate(operation, num1, num2) {
     switch (operation) {
-        case 'add':
+        case '+':
             return add(num1, num2);
-        case 'subtract':
+        case '-':
             return sub(num1, num2);
-        case 'multiply':
+        case 'x':
             return mult(num1, num2);
-        case 'divide':
+        case '/':
             return divide(num1, num2);
-        case 'percent':
-            return percent(num1);
         default:
             break;
     }
 }
 
+decimal.addEventListener('click', () => {
+    equalsClicked = false;
+    if (!(display.textContent.includes('.'))) {
+        display.textContent += '.';
+    }
+})
+
+pct.addEventListener('click', () => {
+    equalsClicked = false;
+    firstNum = display.textContent;
+    display.textContent = firstNum / 100;
+});
+
+del.addEventListener('click', () => {
+    display.textContent = display.textContent.slice(0, -1);
+});
+
+clearAll.addEventListener('click', () => {
+    firstNum = null;
+    secondNum = null;
+    operator = null;
+    display.textContent = '';
+});
+
+equal.addEventListener('click', () => {
+    secondNum = display.textContent;
+    display.textContent = operate(operator, Number(firstNum), Number(secondNum));
+    equalsClicked = true;
+});
+
+
+
 for (let i = 0; i < numberButtons.length; i++) {
     numberButtons[i].addEventListener('click', () => {
-        display.textContent = numberButtons[i].id;
+        if (equalsClicked) {
+            equalsClicked = false;
+            display.textContent = '';
+            display.textContent += numberButtons[i].textContent;
+        } else {
+            display.textContent += numberButtons[i].textContent;
+        }
     })
 }
+for (let i = 0; i < operatorButtons.length; i++) {
+    operatorButtons[i].addEventListener('click', () => {
+        firstNum = display.textContent;
+        operator = operatorButtons[i].id;
+        display.textContent = '';
+    })
+}
+
 
 function add(num1, num2) {
     return num1 + num2;
@@ -59,16 +92,4 @@ function mult(num1, num2) {
 
 function divide(num1, num2) {
     return num1 / num2;
-}
-
-function percentage(num) {
-    return (num / 100)
-}
-
-function backspace() {
-
-}
-
-function clear() {
-
 }
